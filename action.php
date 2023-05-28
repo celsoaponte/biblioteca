ffcabc933e193d45a53616fb8e2923d35983670b
kuey <celsoaponte@outlook.com>
2023-05-27 20:53:02 -0400

    Puedo vizualizar las tablas y agregar autores

--- action.php
+++ action.php
@@ -21,78 +21,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Realizar diferentes acciones según la selección
     switch ($accion) {
         case "visualizar":
-            // Código para visualizar los datos de la tabla seleccionada
-            $sql = "SELECT * FROM $tabla";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    // Accede a los datos de cada fila y muestra los resultados
-                    echo "ID: " . $row["id"] . "<br>";
-                    if ($tabla === "libros") {
-                        echo "Título: " . $row["titulo_libro"] . "<br>";
-                        echo "Autor ID: " . $row["autor_id"] . "<br>";
-                        echo "Editorial ID: " . $row["editorial_id"] . "<br>";
-                        echo "Género ID: " . $row["genero_id"] . "<br>";
-                    } elseif ($tabla === "autores") {
-                        echo "Nombre Autor: " . $row["nombre_autor"] . "<br>";
-                    } elseif ($tabla === "editoriales") {
-                        echo "Nombre Editorial: " . $row["nombre_editorial"] . "<br>";
-                    } elseif ($tabla === "generos") {
-                        echo "Nombre Género: " . $row["nombre_genero"] . "<br>";
-                    } elseif ($tabla === "reviews") {
-                        echo "Libro ID: " . $row["libro_id"] . "<br>";
-                        echo "Calificación: " . $row["calificacion"] . "<br>";
-                        echo "Comentario: " . $row["comentario"] . "<br>";
-                    }
-                    echo "<br>";
-                }
-            } else {
-                echo "No se encontraron registros.";
-            }
-
+            visualizarTabla($conn, $tabla);
             break;
 
         case "insertar":
-            // Código para insertar nuevos datos en la tabla seleccionada
-            if ($tabla === "libros") {
-                $titulo = $_POST["titulo"];
-                $autor_id = $_POST["autor_id"];
-                $editorial_id = $_POST["editorial_id"];
-                $genero_id = $_POST["genero_id"];
-
-                $sql = "INSERT INTO $tabla (titulo_libro, autor_id, editorial_id, genero_id)
-                        VALUES ('$titulo', '$autor_id', '$editorial_id', '$genero_id')";
-            } elseif ($tabla === "autores") {
-                $nombre_autor = $_POST["nombre_autor"];
-
-                $sql = "INSERT INTO $tabla (nombre_autor)
-                        VALUES ('$nombre_autor')";
-            } elseif ($tabla === "editoriales") {
-                $nombre_editorial = $_POST["nombre_editorial"];
-
-                $sql = "INSERT INTO $tabla (nombre_editorial)
-                        VALUES ('$nombre_editorial')";
-            } elseif ($tabla === "generos") {
-                $nombre_genero = $_POST["nombre_genero"];
-
-                $sql = "INSERT INTO $tabla (nombre_genero)
-                        VALUES ('$nombre_genero')";
-            } elseif ($tabla === "reviews") {
-                $libro_id = $_POST["libro_id"];
-                $calificacion = $_POST["calificacion"];
-                $comentario = $_POST["comentario"];
-
-                $sql = "INSERT INTO $tabla (libro_id, calificacion, comentario)
-                        VALUES ('$libro_id', '$calificacion', '$comentario')";
+            // Redirigir a la página correspondiente para insertar datos
+            switch ($tabla) {
+                case "autores":
+                    header("Location: insertar_autores.html");
+                    break;
+                case "editoriales":
+                    header("Location: insertar_editoriales.html");
+                    break;
+                case "generos":
+                    header("Location: insertar_generos.html");
+                    break;
+                case "libros":
+                    header("Location: insertar_libros.html");
+                    break;
+                case "reviews":
+                    header("Location: insertar_reviews.html");
+                    break;
+                default:
+                    echo "Tabla seleccionada inválida.";
+                    break;
             }
-
-            if ($conn->query($sql) === TRUE) {
-                echo "Nuevo registro insertado correctamente.";
-            } else {
-                echo "Error al insertar el registro: " . $conn->error;
-            }
-
             break;
 
         case "modificar":
@@ -107,13 +60,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             } else {
                 echo "Error al actualizar el registro: " . $conn->error;
             }
-
             break;
 
         default:
             // Acción inválida
             echo "Acción inválida seleccionada.";
             break;
+    }
+}
+
+// Función para visualizar los datos de una tabla
+function visualizarTabla($conn, $tabla) {
+    echo "<h2>Tabla: $tabla</h2>";
+
+    // Realizar la consulta a la base de datos y mostrar los resultados
+    $sql = "SELECT * FROM $tabla";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<tr>";
+        while ($row = $result->fetch_assoc()) {
+            // Mostrar los datos de cada fila
+            echo "<tr>";
+            foreach ($row as $key => $value) {
+                echo "<td>$value</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No se encontraron registros en la tabla.";
     }
 }
 ?>
